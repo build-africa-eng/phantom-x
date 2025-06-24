60aa339c73e8a59005119ab5bd7158751bf98a56
build-africa-eng <afrcanfuture@gmail.com>
2025-06-24 21:17:06 +0300

    Update format-code.sh

--- tools/format-code.sh
+++ tools/format-code.sh
@@ -1,4 +1,1 @@
-#!/usr/bin/env bash
-
-cwd=$(pwd)
-clang-format-6.0 -i --style=WebKit $cwd/src/*.h $cwd/src/*.cpp
+CLANG_FORMAT=$(command -v clang-format || command -v clang-format-15 || command -v clang-format-14 || command -v clang-format-13)
