9082745ac1850160165f5e1da1a9c778a86dcd6a
build-africa-eng <afrcanfuture@gmail.com>
2025-06-26 21:46:31 +0300

    Update webpage.h

--- src/core/webpage.h
+++ src/core/webpage.h
@@ -1,33 +1,3 @@
-/*
-  This file is part of the PhantomJS project from Ofi Labs.
-
-  Copyright (C) 2011=2025 Ariya Hidayat <ariya.hidayat@gmail.com>
-  Copyright (C) 2011-2025 Ivan De Marino <ivan.de.marino@gmail.com>
-
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the <organization> nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-  ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 #ifndef WEBPAGE_H
 #define WEBPAGE_H
 
@@ -42,7 +12,7 @@
 #include <QPointer>
 #include <QNetworkRequest>
 #include <QNetworkAccessManager>
-#include <QNetworkProxy> // Ensure this is included
+#include <QNetworkProxy>
 
 #include "ienginebackend.h"
 
@@ -107,9 +77,8 @@ public:
 
     // --- Settings ---
     void applySettings(const QVariantMap& def);
-    // CRITICAL FIX: Make setProxy accept QNetworkProxy and add proxy() getter
-    void setProxy(const QNetworkProxy& proxy); // FIXED: Signature updated
-    QNetworkProxy proxy() const; // FIXED: Added getter for proxy
+    void setProxy(const QNetworkProxy& proxy);
+    QNetworkProxy proxy() const;
     QString userAgent() const;
     void setUserAgent(const QString& ua);
     void setNavigationLocked(bool lock);
@@ -176,10 +145,11 @@ public:
     // --- Internal Callback Handlers ---
     QString filePicker(const QString& oldFile);
     bool javaScriptConfirm(const QString& msg);
-    bool javaScriptPrompt(const QString& msg, const QString& defaultValue, QString* result);
+    bool javaScriptPrompt(
+        const QString& msg, const QString& defaultValue, QString* result);
     void javascriptInterrupt();
 
-    IEngineBackend* engineBackend() const; // Ensure this is PUBLIC
+    IEngineBackend* engineBackend() const;
 
 signals:
     void loadStarted();
